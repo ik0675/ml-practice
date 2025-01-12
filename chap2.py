49b7997e7c74765bc264b2d3f46da6174cd85c6a
Alex Mac Mini <ik0675@utexas.edu>
2025-01-12 00:54:49 -0600

    completed 2-1 problem

--- chap2.py
+++ chap2.py
@@ -1,6 +1,7 @@
 # Try to build a classifier for the MNIST dataset that achieves over 97% accuracy on the test set.
 
-# 1.
+################################################    1    ################################################
+
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.neighbors import KNeighborsClassifier
@@ -13,15 +14,23 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 knn_clf = KNeighborsClassifier()
 knn_clf.fit(X_train, y_train)
 base_accuracy = knn_clf.score(X_test, y_test)
-print(base_accuracy)
 
 from sklearn.model_selection import GridSearchCV
 
 # To speed up the search, let's train only on the first 10,000 images
-grid_cv_params = [{'wights': ['uniform', 'distance'], 'n_neighbors': [3,4,5,6]}]
+grid_cv_params = [{'weights': ['uniform', 'distance'], 'n_neighbors': [3,4,5,6]}]
 
 knn_clf = KNeighborsClassifier()
 grid_srch = GridSearchCV(knn_clf, grid_cv_params, cv = 5)
 grid_srch.fit(X_train[:10_000], y_train[:10_000])
 
 grid_srch.best_params_
+
+grid_srch.best_score_
+
+# Retrain data with the best model (hyperparameter tunned)
+grid_srch.best_estimator_.fit(X_train, y_train)
+tuned_accuracy = grid_srch.score(X_test, y_test)
+print(tuned_accuracy) # Reached over 97%
+
+################################################    2    ################################################
