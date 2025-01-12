2adacab7115fe68b3950a48bceecd41b32f254c6
Alex Mac Mini <ik0675@utexas.edu>
2025-01-12 00:43:30 -0600

    initial commit for chapter 2 activity

---
+++ chap2.py
@@ -0,0 +1,27 @@
+# Try to build a classifier for the MNIST dataset that achieves over 97% accuracy on the test set.
+
+# 1.
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.neighbors import KNeighborsClassifier
+
+mnist = fetch_openml('mnist_784', as_frame=False)
+X, y = mnist.data, mnist.target
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# KNN_classifier
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_train)
+base_accuracy = knn_clf.score(X_test, y_test)
+print(base_accuracy)
+
+from sklearn.model_selection import GridSearchCV
+
+# To speed up the search, let's train only on the first 10,000 images
+grid_cv_params = [{'wights': ['uniform', 'distance'], 'n_neighbors': [3,4,5,6]}]
+
+knn_clf = KNeighborsClassifier()
+grid_srch = GridSearchCV(knn_clf, grid_cv_params, cv = 5)
+grid_srch.fit(X_train[:10_000], y_train[:10_000])
+
+grid_srch.best_params_
